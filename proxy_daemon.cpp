86d08137336359f501aee1206fad4528498cf843
WalterJ726 <walterjiang726@gmail.com>
2023-02-21 12:49:21 -0500

    changed some filenames

--- proxy_daemon.cpp
+++ proxy_daemon.cpp
@@ -12,5 +12,19 @@
 
 
 int main(){
-    
+    LOG(INFO)<<"testDaemon start";
+    if(daemon(1, 0) == -1){
+        std::cout<<"error\n"<<std::endl;
+        exit(-1);
+    }
+
+    LOG(INFO)<<"testDaemon after daemon";
+    int count=0;
+    while (true){
+        sleep(1);
+        LOG(INFO)<<++count<<" : testDaemon ...";
+//        std::cout<<"testDaemon..."<<std::endl;
+    }
+}
+
 }
