7dc3987609d6d1e472499004f892b74cba87c9cb
adolfxuhj <adolfxuhj@163.com>
2023-02-20 23:50:12 -0500

    client & httprequest

--- client.cpp
+++ client.cpp
@@ -1,1 +1,99 @@
-#include <string>
+#include "client.hpp"
+
+Client::Client(unsigned short int port, const char *hostname) : serverPort(port)
+{
+    serverHostname = hostname;
+    try
+    {
+        getAddrinfo();
+        createSocket();
+        createConnect();
+    }
+    catch (const std::exception &e)
+    {
+    }
+    freeaddrinfo(serverAddr);
+}
+int Client::getSockfd() const
+{
+    return sockfd;
+}
+
+void Client::getAddrinfo()
+{
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if (getaddrinfo(serverHostname.c_str(), std::to_string(serverPort).c_str(), &hints, &serverAddr) != 0)
+    {
+        std::perror("failed: client cannot get addr info.");
+        throw std::exception();
+    }
+}
+
+void Client::createSocket()
+{
+    if ((sockfd = socket(serverAddr->ai_family, serverAddr->ai_socktype, serverAddr->ai_protocol)) == -1)
+    {
+        std::perror("failed: client cannot create socket.");
+        throw std::exception();
+    }
+}
+void Client::createConnect()
+{
+    if (connect(sockfd, serverAddr->ai_addr, serverAddr->ai_addrlen) == -1)
+    {
+        perror("failed: client cannot connect server.");
+        throw std::exception();
+    }
+}
+
+void Client::closeClient()
+{
+    if (close(sockfd) != 0)
+    {
+        perror("failed: client cannot close socket.");
+        throw std::exception();
+    }
+}
+
+void Client::sendRequest(void *msg, const size_t size)
+{
+    int numBytes = 0;
+    int recvBytes = 0;
+    while ((numBytes < size))
+    {
+        if ((recvBytes = send(sockfd, msg, size, 0)) == -1)
+        {
+            perror("client send");
+            throw std::exception();
+        }
+        numBytes += recvBytes;
+    }
+}
+
+std::string Client::recvResponse()
+{
+    char buf[MAX_TCP_PACKET_SIZE];
+    int numBytes = 0;
+    if ((numBytes = recv(sockfd, buf, sizeof(buf), 0)) == -1)
+    {
+        perror("client recv");
+        throw std::exception();
+    }
+    buf[numBytes] = '\0';
+
+    return std::string(buf);
+}
+
+Client::~Client()
+{
+    try
+    {
+        closeClient();
+    }
+    catch (const std::exception &e)
+    {
+    }
+}
---
+++ client.hpp
@@ -0,0 +1,34 @@
+#ifndef __CLIENT_HPP__
+#define __CLIENT_HPP__
+#include <iostream>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+#include <exception>
+
+#define MAX_TCP_PACKET_SIZE 65535
+
+class Client
+{
+private:
+    unsigned short int serverPort;
+    std::string serverHostname;
+    int sockfd;
+
+    struct addrinfo *serverAddr;
+
+public:
+    Client(unsigned short int port, const char *hostname) {}
+    int getSockfd() const {}
+    void getAddrinfo() {}
+    void createSocket() {}
+    void createConnect() {}
+    void closeClient() {}
+    void sendRequest(void *msg, const size_t size) {}
+    std::string recvResponse() {}
+    ~Client() {}
+};
+
+#endif
