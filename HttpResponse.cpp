eaaf968088ab4ef116ff4290e24a4c5db30cfeb1
WalterJ726 <walterjiang726@gmail.com>
2023-02-21 22:12:04 -0500

    start write HttpResponse.cpp

---
+++ HttpResponse.cpp
@@ -0,0 +1,125 @@
+#include "httprequest.hpp"
+
+HttpRequest::HttpRequest(const std::string &rawRequest) : httpRequest(rawRequest)
+{
+    try
+    {
+        // TODO: 如果解析报文格式不正确应该在哪里处理，是proxy还是webserver
+        parseStartLine();
+        parseHeaderFields();
+        parseHostAndPort();
+    }
+    catch (const std::exception &e)
+    {
+        // send 400 bad request response (还没实现)
+    }
+
+    // record request and write log
+}
+
+std::string HttpRequest::getMethod() const
+{
+    return method;
+}
+
+std::string HttpRequest::getRequestTarget() const
+{
+    return requestTarget;
+}
+
+std::string HttpRequest::getPort() const
+{
+    return port;
+}
+
+std::string HttpRequest::getHost() const
+{
+    return host;
+}
+
+std::string HttpRequest::getRawRequestText() const
+{
+    return httpRequest;
+}
+
+void HttpRequest::parseStartLine()
+{
+    size_t requestLineEnd = httpRequest.find("\r\n"); // TODO: there is no \r\n in request header
+    std::string requestLine = httpRequest.substr(0, requestLineEnd);
+    std::vector<std::string> requestLineParts;
+    size_t pos = 0;
+    while (pos != std::string::npos)
+    {
+        size_t end = requestLine.find(" ", pos);
+        if (end == std::string::npos)
+        {
+            requestLineParts.push_back(requestLine.substr(pos));
+            break;
+        }
+        requestLineParts.push_back(requestLine.substr(pos, end - pos));
+        pos = end + 1;
+    }
+    if (requestLineParts.size() != 3)
+    {
+        throw std::exception();
+    }
+    if (!(requestLineParts[0] == "GET" || requestLineParts[0] == "POST" || requestLineParts[0] == "CONNECT"))
+    {
+        // method is not correct
+        throw std::exception();
+    }
+    method = requestLineParts[0];
+    requestTarget = requestLineParts[1];
+    httpVersion = requestLineParts[2];
+}
+
+void HttpRequest::parseHeaderFields()
+{
+    size_t requestLineEnd = httpRequest.find("\r\n");
+    size_t headerEnd = httpRequest.find("\r\n\r\n");
+    std::string headers = httpRequest.substr(requestLineEnd + 2, headerEnd - requestLineEnd - 2);
+    size_t pos = 0;
+    while (true)
+    {
+        size_t end = headers.find("\r\n", pos);
+        std::string line = headers.substr(pos, end - pos);
+        pos = end + 2;
+
+        size_t colon_pos = line.find(":");
+        if (colon_pos != std::string::npos)
+        {
+            std::string key = line.substr(0, colon_pos);
+            std::string value = line.substr(colon_pos + 2); // +2 to skip ": "
+            headerMap[key] = value;
+        }
+        if (end == std::string::npos)
+        {
+            break;
+        }
+    }
+}
+
+void HttpRequest::parseHostAndPort()
+{
+    if (headerMap.count("Host") <= 0)
+    {
+        throw std::exception();
+    }
+    std::string Host = headerMap["Host"];
+    size_t pos = Host.find(":");
+    if (pos != std::string::npos)
+    {
+        host = Host.substr(0, pos);
+        port = Host.substr(pos + 1);
+        if (port.empty())
+        {
+            port = "80";
+        }
+    }
+    else
+    {
+        host = Host;
+        port = "80";
+    }
+    // 如果Host为空按理说是不需要处理的
+}
---
+++ HttpResponse.hpp
@@ -0,0 +1,41 @@
+#ifndef __HTTPREQUEST_HPP__
+#define __HTTPREQUEST_HPP__
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+/* danger log */
+// 1.请求体可能是有误的，没有考虑request存在问题的情况
+// 2.需要分解host的端口号， host中可能会带有端口号而非默认的80
+
+class HttpRequest
+{
+private:
+    std::string httpRequest;
+    std::string method;
+    std::string requestTarget;
+    std::string httpVersion;
+    std::string port;
+    std::string host;
+    std::map<std::string, std::string> headerMap;
+
+public:
+    HttpRequest(const std::string &rawRequest);
+    std::string getMethod() const;
+    std::string getRequestTarget() const;
+    std::string getPort() const;
+    std::string getHost() const;
+    std::string getRawRequestText() const;
+    void parseStartLine();
+    void parseHeaderFields();
+    void parseHostAndPort();
+};
+
+// 1.验证方法是否是GET POST CONNECT中的一种, 不是就返回405 method not allowed
+// 2.在http 1.1中不能缺失host字段, 如果缺失, 服务器返回400 bad request
+
+#endif
